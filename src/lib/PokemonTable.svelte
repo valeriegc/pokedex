2c0e2966bbb7f3a7aec9f6eb80ffa03de6eae5cc
Valerie Knape <valerieknape@gmail.com>
2023-05-26 16:00:23 +0200

    initial commit

---
+++ src/lib/PokemonTable.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	interface PokeData {
+		name: string;
+		number: number;
+		type: string;
+		hp: number;
+	}
+	export let pokeArr: PokeData[];
+</script>
+
+<div class="table-container mt-14">
+	<!-- Native Table Element -->
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Number</th>
+				<th>Type</th>
+				<th>HP</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each pokeArr as pokemon, i}
+				<tr>
+					<td>{pokemon.name}</td>
+					<td>{pokemon.number}</td>
+					<td>{pokemon.type}</td>
+					<td>{pokemon.hp}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import PokemonTable from '$lib/PokemonTable.svelte';
+	const Pokemon = [
+		{
+			name: 'Alexsaur',
+			number: 34,
+			type: 'Dorkemon',
+			hp: 1000
+		}
+	];
+</script>
+
+<div class="px-5 max-w-4xl mx-auto">
+	<h1 class="text-center text-7xl">Pokedex</h1>
+	<div class="flex justify-center mt-7">
+		<input class="input w-72" title="Input (text)" type="text" placeholder="Search for a pokemon" />
+		<button type="button" class="btn-icon variant-filled ml-2">(icon)</button>
+	</div>
+	<PokemonTable pokeArr={Pokemon} />
+</div>
+
+<style lang="postcss">
+</style>
